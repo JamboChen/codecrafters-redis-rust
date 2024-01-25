baefb157297878fcef9a8cfc57af4dfb7b2c6f36
shaodong csqy <chenjambo@gmail.com>
2024-01-25 19:01:00 +0800

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
-use std::net::TcpListener;
+use std::{
+    io::Write,
+    net::{TcpListener, TcpStream},
+};
+
+fn execute_command(stream: TcpStream) {
+    let resp = "+PONG\r\n";
+    let mut stream = stream;
+    stream.write(resp.as_bytes()).expect("write failed");
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
+
     for stream in listener.incoming() {
         match stream {
             Ok(_stream) => {
                 println!("accepted new connection");
+                execute_command(_stream)
             }
             Err(e) => {
                 println!("error: {}", e);
