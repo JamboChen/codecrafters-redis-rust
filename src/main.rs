390b54e708b5b1dd1056ba68607726a2b1e323b3
shaodong csqy <chenjambo@gmail.com>
2024-01-25 20:43:05 +0800

    Refactor main.rs to use async/await syntax

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,35 @@
-use std::{
-    io::{Error, Read, Write},
+use std::io::Error;
+
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
 };
 
-fn execute_command(stream: TcpStream) -> Result<(), Error> {
+async fn execute_command(stream: TcpStream) -> Result<(), Error> {
     let response = "+PONG\r\n";
     let mut stream = stream;
     let mut buf = [0; 1024];
-    while let Ok(_) = stream.read(&mut buf) {
-        stream.write(response.as_bytes())?;
+    while let Ok(n) = stream.read(&mut buf).await {
+        if n == 0 {
+            break;
+        }
+        stream.write_all(response.as_bytes()).await?;
     }
     Ok(())
 }
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379")
+        .await
+        .expect("failed to bind");
 
-    for stream in listener.incoming() {
+    loop {
+        let stream = listener.accept().await;
         match stream {
-            Ok(_stream) => {
+            Ok((_stream, _)) => {
                 println!("accepted new connection");
-                if let Err(e) = execute_command(_stream) {
+                if let Err(e) = execute_command(_stream).await {
                     println!("error: {}", e);
                 }
             }
